45a939ef7fdc4c192741d206ee408d453258f4cf
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-03 06:46:42 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Cder(models.Model):
+    'Generated Model'
+    nvnvn = models.BigIntegerField()
+    oyii = models.BigIntegerField()
